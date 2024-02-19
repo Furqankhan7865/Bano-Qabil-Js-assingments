d4c9153535fcd83927f241b06200ebd3017c76cb
Furqankhan7865 <153278668+Furqankhan7865@users.noreply.github.com>
2024-02-18 20:33:29 -0800

    Add files via upload

---
+++ Assignment 1/app.js
@@ -0,0 +1,140 @@
+//Question#1//
+// alert("Hello Welcome to our website")
+//Question#2//
+// alert("Error! Please enter Valid Password")
+//Question#3//
+// var message = "Welcome To js.\nWorkhard For Better Work.";
+// alert(message)
+//Question#4//
+// // Define an array of messages
+// var messages = [
+//     "Welcome To Js world",
+//     "Try to Improve",
+//     "Try to Workhard"
+// ];
+
+// // Function to display messages in sequence
+// function displayMessagesSequentially(messages, index) {
+//     // Check if index is within bounds
+//     if (index < messages.length) {
+//         // Display the message
+//         alert(messages[index]);
+//         // Increment index to display the next message after a delay
+//         setTimeout(function() {
+//             displayMessagesSequentially(messages, index + 1);
+//         }, 1000); // Delay in milliseconds (1000 milliseconds = 1 second)
+//     }
+// }
+
+// // Start displaying messages, starting from index 0
+// displayMessagesSequentially(messages, 0);
+//Questtion5//
+// function displayMessage() {
+    // alert("Welcome To java Script");
+// }
+//Question6//
+//Question7//
+// var username;
+// var username = "example_username";
+//Question8//
+// var myName = "Whats your Name";
+// var myName = "Muhammad Furqan Khan";
+    //Question9//
+    // var message;
+    // Message="Hello World";
+    //Question10//
+// var fullName = "Muhammad Furqan khan";
+// var age = 19;
+// var gender = "Male";
+// var address = "Gulistan e jouhar karachi";
+// var email = "Furqankhan106508@gmail.com";
+// var phoneNumber = "03021594844";
+// alert("Full Name: " + fullName);
+// alert("Age: " + age);
+// alert("Gender: " + gender);
+// alert("Address: " + address);
+// alert("Email: " + email);
+// alert("Phone Number: " + phoneNumber);
+ //Quesion11//
+//  var email = "example@example.com";
+//  alert("My email address is: " + email);
+//Question12//
+// window.onload = function() {
+//     var displayDiv = document.getElementById("displayText");
+//     var textToDisplay = "Hello, world! This is displayed through JavaScript.";
+//     displayDiv.innerHTML = textToDisplay; 
+// }   
+//question#13//
+// var myString = "Name=Furqan";
+// alert(mystring);
+// document.write("<p>" + myString + "</p>");
+//Question#14//
+// var age = 25;
+// alert("Your age is: " + age);
+//Question#15//
+// - function updateVisitCount() {
+
+//     if (typeof(Storage) !== "undefined") {
+
+//         if (localStorage.visitCount) {
+    
+//             localStorage.visitCount = Number(localStorage.visitCount) + 1;
+//         } else {
+        
+//             localStorage.visitCount = 1;
+//         }
+        
+//         document.getElementById("visitCount").innerText = localStorage.visitCount;
+//     } else {
+        
+//         document.getElementById("visitCount").innerText = "Sorry, your browser doesn't support web storage.";
+//     }
+// }
+
+// window.onload = updateVisitCount;
+//Queestion16//
+// var birthYear = 2004;
+
+// window.onload = function() {
+//     alert("Your birth year is: " + birthYear);
+// };
+//Question17//
+// var visitorName = "Ali Baig";
+// var productTitle = "T-shirt";
+// var quantity = 5;
+
+// // Message to display in the browser
+// var message = visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store";
+
+// // Display the message in the browser
+// window.onload = function() {
+//     alert(message);
+// }
+//question18//
+// var variable1, variable2, variable3;
+//Question19//
+// firstName
+// _age
+// $totalAmount
+// user123
+// myVar
+//  names:
+
+// 123user 
+// my-variable 
+// var
+// my variable
+// my@var 
+//Question20//
+//Question22//
+// var num1 = 5;
+// var num2 = 7;
+// var sum = num1 + num2;
+
+// document.write("The sum of " + num1 + " and " + num2 + " is: " + sum);
+//Question23//
+var num1 = 5;
+var num2 = 7;
+var sum = num1 + num2;
+
+document.write("The sum of " + num1 + " and " + num2 + " is: " + sum);
